e6cb69f05db6f14126c076c91b4d84b2fd4db8e1
SherbakovSergey <sergey51979@mail.ru>
2020-08-04 03:38:52 +0300

    5.4. Протеиновая диета [#331847]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+    @Test
+    public void whenIvanGreatNik1Month() {
+        int month = Fitness.calc(90, 95);
+        assertThat(month, is(1));
+    }
+
+    @Test
+    public void whenIvanGreatNik2Month() {
+        int month = Fitness.calc(25, 50);
+        assertThat(month, is(2));
+    }
+
+    @Test
+    public void whenIvanGreatNik3Month() {
+        int month = Fitness.calc(20, 60);
+        assertThat(month, is(3));
+    }
+}
