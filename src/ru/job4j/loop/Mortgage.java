bb90b1db9bf2a594a8d8a455f8b82e1d8c3af852
SherbakovSergey <sergey51979@mail.ru>
2020-08-05 22:44:55 +0300

    5.7. Ипотека [#331849]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,11 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
-        int year = 1;
-        double remainingDebt = amount * percent + amount - salary;
-        while (remainingDebt > 0) {
+        int year = 0;
+        while (amount > 0) {
             year += 1;
-            remainingDebt = remainingDebt * percent + remainingDebt - salary;
+            amount = (int) (amount * percent + amount - salary);
         }
         return year;
     }
