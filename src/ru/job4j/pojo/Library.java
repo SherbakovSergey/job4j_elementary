c30619ef31daf2960266be730f5d0483b994b8c6
SherbakovSergey <sergey51979@mail.ru>
2020-09-03 22:10:42 +0300

    3. Массивы и модели. [#331972]

--- src/ru/job4j/pojo/Library.java
+++ src/ru/job4j/pojo/Library.java
@@ -4,24 +4,27 @@ public class Library {
     public static void main(String[] args) {
         Book book1 = new Book("Book1", 100);
         Book book2 = new Book("Book2", 200);
-        Book book3 = new Book("Book3", 300);
+        Book book3 = new Book("Clean code", 300);
         Book book4 = new Book("Book4", 400);
-        Book[] cleanCode = new Book[4];
-        cleanCode[0] = book1;
-        cleanCode[1] = book2;
-        cleanCode[2] = book3;
-        cleanCode[3] = book4;
-        for (int i = 0; i < cleanCode.length; i++) {
-            Book bk = cleanCode[i];
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
             System.out.println(bk.getName() + " " + bk.getPage());
         }
-        Book temp = cleanCode[0];
-        cleanCode[0] = cleanCode[3];
-        cleanCode[3] = temp;
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
         System.out.println("Clean code");
-        for (int i = 0; i < cleanCode.length; i++) {
-            Book bk = cleanCode[i];
-            System.out.println(bk.getName() + " " + bk.getPage());
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            if (bk.equals("Clean code")) {
+                System.out.println(bk.getName() + " " + bk.getPage());
+                break;
+            }
         }
     }
 }
